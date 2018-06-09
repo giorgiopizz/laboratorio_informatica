6c838c4480683360dd7fa160beebf77e2a784160
Giorgio Pizzati <g.pizzati@campus.unimib.it>
2018-06-09 22:14:11 +0200

    ultimo esercizio

---
+++ esercitazione 8/es83.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define DIM 100
+void som(int som[],int primo[], int secondo[]){
+    int i;
+    if(primo[0]>=secondo[0]){
+        int diff=primo[0]-secondo[0];
+        for(i=primo[0];i>0;--i){
+            if((i-diff)>0){
+                som[i]=primo[i]+secondo[i-diff];
+            }
+            else{
+                som[i]=primo[i];
+            }
+            
+        }
+        som[0]=primo[0];
+    }
+    else{
+        int diff=secondo[0]-primo[0];
+        for(i=secondo[0];i>0;--i){
+            if((i-diff)>0){
+                som[i]=primo[i-diff]+secondo[i];
+            }
+            else{
+                som[i]=secondo[i];
+            }
+            
+        }
+        som[0]=secondo[0];
+    }
+}
+void printa(int vect[]){
+    int i;
+    for(i=1;i<=vect[0];++i){
+        printf("%i", vect[i]);
+    }
+}
+int main(){
+    int primo[DIM], secondo[DIM];
+    char c;
+    c=getchar();
+    int i=1;
+    while(c!='\n'){
+        if(c>='0' && c<='9'){
+            primo[i]=c-'0';
+            ++i;
+            c=getchar();
+        }
+    }
+    primo[0]=i-1;
+    i=1;
+    c=getchar();
+    while(c!='\n'){
+        if(c>='0' && c<='9'){
+            secondo[i]=c-'0';
+            ++i;
+            c=getchar();
+        }
+    }
+    secondo[0]=i-1;
+    int somma[DIM];
+    som(somma,primo,secondo);
+    printa(somma);
+}
