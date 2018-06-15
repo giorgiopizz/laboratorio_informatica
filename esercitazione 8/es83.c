f2d846fdc3748a3cb93808a0d8985d3337509b42
Giorgio Pizzati <g.pizzati@campus.unimib.it>
2018-06-15 14:55:15 +0200

    vjfj

--- esercitazione 8/es83.c
+++ esercitazione 8/es83.c
@@ -30,6 +30,37 @@ void som(int som[],int primo[], int secondo[]){
         som[0]=secondo[0];
     }
 }
+void prodotto(int prod[], int primo[], int secondo[]){
+    int i, resto=0, num;
+    if(primo[0]>=secondo[0]){
+        int diff=primo[0]-secondo[0];
+        for(i=primo[0];i>0;--i){
+            if((i-diff)>0){
+                num=primo[i]*secondo[i-diff]+resto;
+                resto=num/10;
+                prod[i]=num
+            }
+            else{
+                som[i]=primo[i];
+            }
+            
+        }
+        som[0]=primo[0];
+    }
+    else{
+        int diff=secondo[0]-primo[0];
+        for(i=secondo[0];i>0;--i){
+            if((i-diff)>0){
+                som[i]=primo[i-diff]+secondo[i];
+            }
+            else{
+                som[i]=secondo[i];
+            }
+            
+        }
+        som[0]=secondo[0];
+    }
+}
 void printa(int vect[]){
     int i;
     for(i=1;i<=vect[0];++i){
